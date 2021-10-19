f20cae7739c93188ff92a18341b404ac1cc86e86
Yona Dav <yonadav.le@gmail.com>
2021-10-20 00:38:26 +0300

    add pearson function to anomaly_detection_util.cpp

--- anomaly_detection_util.cpp
+++ anomaly_detection_util.cpp
@@ -4,9 +4,10 @@
 #include <stdio.h>
 #include <iostream>
 #include "anomaly_detection_util.h"
+#include <cmath>
 
 // returns average of floats
-float avg(float* x, int size){
+float avg(float *x, int size) {
     float sum = 0;
     for (int i = 0; i < size; ++i) {
         sum += x[i];
@@ -15,21 +16,19 @@ float avg(float* x, int size){
 }
 
 
-
 int main() {
     float nums[] = {10, 20, 30, 40};
     float nums2[] = {55, 76, 27, 35};
     float variance = var(nums, 4);
-    float covariance = cov(nums,nums2,4);
+    float covariance = cov(nums, nums2, 4);
     printf("%f", variance);
-   // printf("%f", covariance);
+    // printf("%f", covariance);
     return 1;
 }
 
 
-
 // returns the variance of X and Y
-float var(float* x, int size){
+float var(float *x, int size) {
     float average = avg(x, size);
     float sum = 0;
     for (int i = 0; i < size; ++i) {
@@ -37,12 +36,27 @@ float var(float* x, int size){
     }
     return sum / size;
 }
+
 // returns the covariance of X and Y
-float cov(float* x, float* y, int size){
+float cov(float *x, float *y, int size) {
     float sum = 0;
-    for(int i = 0; i < size; i++)
-        sum = sum + (x[i] - avg(x,size)) *
+    for (int i = 0; i < size; i++)
+        sum = sum + (x[i] - avg(x, size)) *
                     (y[i] - avg(x, size));
+
     return sum / (size - 1);
+}
+
+// returns the Pearson correlation coefficient of X and Y
+float pearson(float *x, float *y, int size) {
+    float covariance = cov(x, y, size);
+    float variance_x = var(x, size);
+    float variance_y = var(y, size);
+    float deviation_x = sqrt(variance_x);
+    float deviation_y = sqrt(variance_y);
+    return (covariance / (deviation_x * deviation_y));
+
 
 }
+
+
