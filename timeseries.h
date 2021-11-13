76f0298930e3e0698c52f419feb6f6532f65714e
Itay Elyashev <itay.elyashiv@gmail.com>
2021-11-13 19:14:24 +0200

    added little to timeseries.h constructor

--- timeseries.h
+++ timeseries.h
@@ -3,13 +3,25 @@
 #ifndef TIMESERIES_H_
 #define TIMESERIES_H_
 
+#include <vector>
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+#include <fstream>
+
 using namespace std;
 
 class TimeSeries{
-
+    vector<string> names;
+    vector<vector<float>> values;
 public:
-
 	TimeSeries(const char* CSVfileName){
+        // File pointer
+        fstream fin;
+        // Open an existing file
+        fin.open(CSVfileName, ios::in);
+
+
 	}
 
 };
